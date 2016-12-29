476c97edf8489be59d5e96ce36aa9214ae4ca00c
Loren Oh <loren.gh.oh@gmail.com>
2016-12-29 14:27:57 -0800

    Read model parameters and output at the beginning

--- run_tracker.py
+++ run_tracker.py
@@ -1,24 +1,22 @@
 import sys, json
 
-from cloudtracker import main as tracker_main
+from cloudtracker.main import main
+from cloudtracker.load_config import config 
+from cloudtracker.load_config import c
     
-def run_tracker(input):
+def run_tracker():
     print( " Running the cloud-tracking algorithm... " )
-    print( " Input dir: \"" + input + "\" \n" )
 
-    # Read .json configuration file
-    with open('model_config.json', 'r') as json_file:
-        config = json.load(json_file)
+    # Print out model parameters from config.json
+    print( " \n Model parameters: " )
+    print( " \t Case name: {}".format(config.case_name) )
+    print( " \t Model Domain {}x{}x{}".format(c.nx, c.ny, c.nz) )
+    print( " \t {} model time steps \n".format(c.nt) )
 
-    tracker_main.main(input, config)
+    main()
 
     print( "\n Entrainment analysis completed " )
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        run_tracker("./data/")
-    elif len(sys.argv) == 2:
-        run_tracker(sys.argv[1])
-    else:
-        print( " Invalid input " ) 
+    run_tracker()
     
