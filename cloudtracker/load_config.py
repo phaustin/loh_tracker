3da1e7058195ec266c0ae0ffab37fa7e46930086
Loren Oh <loren.gh.oh@gmail.com>
2016-12-29 14:12:47 -0800

    Load JSON configuration parameters

---
+++ cloudtracker/load_config.py
@@ -0,0 +1,15 @@
+import json
+
+class attrdict(dict):
+    def __init__(self, *args, **kwargs):
+        dict.__init__(self, *args, **kwargs)
+        self.__dict__ = self
+
+with open('model_config.json', 'r') as file:
+    try:
+        config = attrdict(json.load(file))
+    except:
+        raise("JSON config dictionary error.")
+
+    case_name = config['case_name']
+    c = attrdict(config['config'])
