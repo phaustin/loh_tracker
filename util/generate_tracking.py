541bceba7ca43653c7b934c5d4ba4acf13753273
Loren Oh <loren.gh.oh@gmail.com>
2017-05-18 14:00:29 -0700

    Pythonic implementation of generate_tracking.py using xarray

--- util/generate_tracking.py
+++ util/generate_tracking.py
@@ -1,28 +1,15 @@
-import os, sys, glob, json
+import os, sys, glob
 import numpy as np
+import json
 
-from netCDF4 import Dataset
+import xarray as xr
+from netCDF4 import Dataset as nc
 
 cp = 1004.    # Heat capacity at constant pressure for dry air [J kg^-1 K^-1]
-cpv = 1870.    # Heat capacity at constant pressure of water vapor [J kg^-1 K^-1]
-Cl = 4190.     # Heat capacity of liquid water [J kg^-1 K^-1]
 Rv = 461.      # Gas constant of water vapor [J kg^-1 K^-1]
 Rd = 287.      # Gas constant of dry air [J kg^-1 K^-1]
-Lv = 2.5104e6 # Latent heat of vaporization [J kg^-1]
-Lf = 0.3336e6  # Latent heat of fusion [J kg^-1]
-Ls = 2.8440e6  # Latent heat of sublimation [J kg^-1]
-g = 9.81       # Accelleration of gravity [m s^-2]
 p_0 = 100000.
-epsilon = Rd/Rv
 lam = Rv/Rd - 1.
-lam = 0.61
-
-def memory_usage():
-    # return the memory usage in MB
-    import psutil
-    process = psutil.Process(os.getpid())
-    mem = process.memory_info()[0] / float(2 ** 20)
-    return mem
 
 def T_v(T, qv, qn, qp):
     return T*(1. + lam*qv - qn - qp)
@@ -32,89 +19,69 @@ def theta(p, T): return T*(p_0/p)**(Rd/cp)
 def theta_v(p, T, qv, qn, qp):
     return theta(p, T_v(T, qv, qn, qp))
 
-def generate_tracking(time_step, file_name):
-    print(file_name)
-    vars = {'core', 'condensed', 'plume', 'u', 'v', 'w'}
+def generate_tracking(time, file):
+    with xr.open_dataset(file) as f:
+        try:
+            f = f.squeeze('time')
+        except:
+            pass
+        print(f)
+
+        print("\t Calculating velocity fields...")
+        w_field = f['W'][:]
+        w_field = (w_field + np.roll(w_field, 1, axis=0)) / 2
+
+        u_field = f['U'][:]
+        w_field = (w_field + np.roll(u_field, 1, axis=1)) / 2
+
+        v_field = f['V'][:]
+        w_field = (w_field + np.roll(v_field, 1, axis=2)) / 2
+
+        print("\t Calculating buoynacy fields...")
+        qn_field = f['QN'][:] / 1e3
+        thetav_field = theta_v(f['p'][:]*100, f['TABS'][:], 
+                               f['QV'][:] / 1e3, qn_field, 0)
+        buoy_field = (thetav_field > 
+                      np.mean(thetav_field, axis=(1,2)))
+
+        print("\t Calculating tracer fields...")
+        tr_field = np.array(f['TR01'][:])
+        tr_mean = np.mean(tr_field.reshape((len(f.z), len(f.y)*len(f.x))), axis=1)
+        tr_stdev = np.sqrt(tr_field.reshape((len(f.z), len(f.y)*len(f.x))).var(1))
+        tr_min = .05 * np.cumsum(tr_stdev)/(np.arange(len(tr_stdev))+1)
+
+        #---- Dataset for storage 
+        print("\t Saving DataArrays...")
+        ds = xr.Dataset(coords= {'z': f.z, 'y':f.y, 'x':f.x})
+
+        mask = (qn_field > 1e-4) | (w_field > 0.2)
+        ds['u'] = u_field.where(mask)
+        ds['v'] = v_field.where(mask)
+        ds['w'] = w_field.where(mask)
     
-    nc_file = Dataset(file_name)
-    save_file = Dataset('data/cloudtracker_input_%08g.nc' % time_step, 'w')
+        ds['core'] = (w_field > 0.) * (buoy_field > 0.) & (qn_field > 1e-4)
+        ds['condensed'] = (qn_field > 1e-4)
+        ds['plume'] = xr.DataArray(tr_field > 
+                       np.max(np.array([tr_mean + tr_stdev, tr_min]), 0)[:, None, None],
+                       dims=['z', 'y', 'x'])
 
-    x = nc_file['x'][:]
-    y = nc_file['y'][:]
-    z = nc_file['z'][:]
-
-    save_file.createDimension('x', len(x))
-    save_file.createDimension('y', len(y))
-    save_file.createDimension('z', len(z))
-
-    x_var = save_file.createVariable('x', 'f', ('x',))
-    x_var[:] = x[:]
-    y_var = save_file.createVariable('y', 'f', ('y',))
-    y_var[:] = y[:]
-    z_var = save_file.createVariable('z', 'f', ('z',))
-    z_var[:] = z[:]
-
-    variables = {}
-    for var_name in vars:
-        if (var_name == {'core', 'condensed', 'plume'}):
-            variables[var_name] = save_file.createVariable(var_name, 'i', ('z', 'y', 'x'), zlib = True)
-        else:
-            variables[var_name] = save_file.createVariable(var_name, 'f', ('z', 'y', 'x'), zlib = True)
-
-    print("\t Open files for writing,", memory_usage())
-    print("\t Calculating velocities...")
-
-    temp_field = nc_file['U'][:, :, :].astype(np.float)
-    temp_field[:, :-1, :] += temp_field[:, 1:, :]
-    temp_field[:, -1, :] += temp_field[:, 0, :]
-    variables['u'][:] = temp_field/2.
-
-    temp_field = nc_file['V'][:, :, :].astype(np.float)
-    temp_field[:, :, :-1] += temp_field[:, :, 1:]
-    temp_field[:, :, -1] += temp_field[:, :, 0]
-    variables['v'][:] = temp_field/2.
-
-    #    print "Load w"
-    temp_field = nc_file['W'][:, :, :].astype(np.float)
-    temp_field[:-1, :, :] += temp_field[1:, :, :]
-    temp_field[:-1, :, :] = temp_field[:-1, :, :]/2.
-    variables['w'][:] = temp_field
-
-    qn_field = nc_file['QN'][:, :, :].astype(np.float)/1000.
-    thetav_field = theta_v((nc_file['p'][:].astype(np.float)*100.)[:, np.newaxis, np.newaxis],
-                               nc_file['TABS'][:, :, :].astype(np.float), \
-                               nc_file['QV'][:, :, :].astype(np.float)/1000., \
-                               qn_field, nc_file['QP'][:, :, :].astype(np.float)/1000.)
-    print("\tLoad thermodynamic fields", memory_usage())
-                     
-    buoy_field = (thetav_field > 
-         (thetav_field.mean(2).mean(1))[:, np.newaxis, np.newaxis])
-    print("\tBuoyancy field,", memory_usage())
-
-    variables['core'][:] = (temp_field > 1.) & buoy_field & (qn_field > 1.e-4)
-    variables['condensed'][:] = (qn_field > 1.e-4)
-    print("\tSave core/condensed region", memory_usage())
-
-    # Tracer fields
-    tr_field = nc_file['TR01'][0, :].astype(np.double)
-    tr_mean = tr_field.reshape((len(z), len(y)*len(x))).mean(1)
-    tr_stdev = np.sqrt(tr_field.reshape((len(z), len(y)*len(x))).var(1))
-    tr_min = .05 * np.cumsum(tr_stdev)/(np.arange(len(tr_stdev))+1)
-    
-    variables['plume'][:] = (tr_field > np.max(np.array([tr_mean + tr_stdev, tr_min]), 0)[:, None, None])
-
-    save_file.close()
-    nc_file.close()
+        # Flag for netCDF compression (very effective for large, sparse arrays)
+        encoding = {}
+        if False:
+            encoding = {var: dict(zlib=True) for var in ds.data_vars}
+        ds.to_netcdf('data/cloudtracker_input_%08g.nc' % time, encoding=encoding)
+        print(ds)
 
 def main():
     global model_config
-    with open('model_config.json', 'r') as json_file:
+    with open('config.json', 'r') as json_file:
         model_config = json.load(json_file)
 
     filelist = sorted(glob.glob('%s/*.nc' % model_config['variables']))
-    for time_step, file_name in enumerate(filelist):
-      print("time_step: " + str(time_step))
-      generate_tracking(time_step, file_name)
+    for time in range(len(filelist)):
+        print('\t Working...%s/%s                        ' % (time, 180), end='\r')
+        generate_tracking(time, filelist[time])
+        break
 
 if __name__ == "__main__":
     main()
